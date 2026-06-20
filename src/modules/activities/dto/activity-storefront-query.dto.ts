35d4c46c875cf836905babe8411628a6aba61931
Kiran Kumar Sabhapathi <39983884+kkversile@users.noreply.github.com>
2026-06-21 01:20:35 +0530

    book activities storefront flow (#239)

---
+++ src/modules/activities/dto/activity-storefront-query.dto.ts
@@ -0,0 +1,34 @@
+import { Type } from 'class-transformer';
+import { IsInt, IsOptional, IsString, Max, Min } from 'class-validator';
+
+export class ActivityStorefrontQueryDto {
+  @IsString()
+  @IsOptional()
+  destination?: string;
+
+  @IsString()
+  @IsOptional()
+  activityType?: string;
+
+  @IsString()
+  @IsOptional()
+  q?: string;
+
+  @IsString()
+  @IsOptional()
+  date?: string;
+
+  @Type(() => Number)
+  @IsInt()
+  @Min(1)
+  @Max(50)
+  @IsOptional()
+  guests?: number;
+
+  @Type(() => Number)
+  @IsInt()
+  @Min(1)
+  @Max(50)
+  @IsOptional()
+  limit?: number;
+}
